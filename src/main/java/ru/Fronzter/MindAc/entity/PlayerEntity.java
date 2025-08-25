dbfb600b4df62e3b66c157128eceb568360a1958
Denis FronZeterov <oliskidenis999@gmail.com>
2025-08-25 19:40:24 +0300

    UPDATE

--- src/main/java/ru/Fronzter/MindAc/entity/PlayerEntity.java
+++ src/main/java/ru/Fronzter/MindAc/entity/PlayerEntity.java
@@ -2,9 +2,7 @@ package ru.Fronzter.MindAc.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public final class PlayerEntity {
@@ -12,11 +10,10 @@ public final class PlayerEntity {
     private final String name;
     private float lastYaw = 0.0F;
     private float lastPitch = 0.0F;
-
     private final List<Frame> mlFrames = new CopyOnWriteArrayList<>();
-    private final Map<Long, Integer> violationLogs = new ConcurrentHashMap<>();
     private List<Frame> lastAnalyzedFrames = null;
     private int combatRecordingTicks = 0;
+    private boolean isProcessingFlag = false;
 
     public PlayerEntity(UUID uuid, String name) {
         this.uuid = uuid;
@@ -30,9 +27,10 @@ public final class PlayerEntity {
     public float getLastPitch() { return this.lastPitch; }
     public void setLastPitch(float lastPitch) { this.lastPitch = lastPitch; }
     public List<Frame> getFrames() { return this.mlFrames; }
-    public Map<Long, Integer> getViolationLogs() { return this.violationLogs; }
     public List<Frame> getLastAnalyzedFrames() { return this.lastAnalyzedFrames; }
     public void setLastAnalyzedFrames(List<Frame> frames) { this.lastAnalyzedFrames = new ArrayList<>(frames); }
     public int getCombatRecordingTicks() { return this.combatRecordingTicks; }
     public void setCombatRecordingTicks(int ticks) { this.combatRecordingTicks = ticks; }
+    public boolean isProcessingFlag() { return isProcessingFlag; }
+    public void setProcessingFlag(boolean processingFlag) { isProcessingFlag = processingFlag; }
 }
